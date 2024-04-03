3a27b9e7d8f3aec50425c09385601ccd65224fc1
WarriorsSami <sbarbutdica@gmail.com>
2024-04-03 17:49:05 +0300

    feat: add basic web browser functionality

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,17 +29,144 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.mainSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.backwardButton = new System.Windows.Forms.Button();
+            this.forwardButton = new System.Windows.Forms.Button();
+            this.homeButton = new System.Windows.Forms.Button();
+            this.goButton = new System.Windows.Forms.Button();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).BeginInit();
+            this.mainSplitContainer.Panel1.SuspendLayout();
+            this.mainSplitContainer.Panel2.SuspendLayout();
+            this.mainSplitContainer.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // mainSplitContainer
+            // 
+            this.mainSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainSplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.mainSplitContainer.Name = "mainSplitContainer";
+            this.mainSplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // mainSplitContainer.Panel1
+            // 
+            this.mainSplitContainer.Panel1.Controls.Add(this.refreshButton);
+            this.mainSplitContainer.Panel1.Controls.Add(this.backwardButton);
+            this.mainSplitContainer.Panel1.Controls.Add(this.forwardButton);
+            this.mainSplitContainer.Panel1.Controls.Add(this.homeButton);
+            this.mainSplitContainer.Panel1.Controls.Add(this.goButton);
+            this.mainSplitContainer.Panel1.Controls.Add(this.addressTextBox);
+            // 
+            // mainSplitContainer.Panel2
+            // 
+            this.mainSplitContainer.Panel2.Controls.Add(this.webBrowser);
+            this.mainSplitContainer.Size = new System.Drawing.Size(800, 450);
+            this.mainSplitContainer.SplitterDistance = 68;
+            this.mainSplitContainer.TabIndex = 0;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(416, 18);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(75, 34);
+            this.refreshButton.TabIndex = 6;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // backwardButton
+            // 
+            this.backwardButton.Location = new System.Drawing.Point(497, 18);
+            this.backwardButton.Name = "backwardButton";
+            this.backwardButton.Size = new System.Drawing.Size(57, 34);
+            this.backwardButton.TabIndex = 5;
+            this.backwardButton.Text = "<";
+            this.backwardButton.UseVisualStyleBackColor = true;
+            this.backwardButton.Click += new System.EventHandler(this.backwardButton_Click);
+            // 
+            // forwardButton
+            // 
+            this.forwardButton.Location = new System.Drawing.Point(560, 18);
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Size = new System.Drawing.Size(57, 34);
+            this.forwardButton.TabIndex = 3;
+            this.forwardButton.Text = ">";
+            this.forwardButton.UseVisualStyleBackColor = true;
+            this.forwardButton.Click += new System.EventHandler(this.forwardButton_Click);
+            // 
+            // homeButton
+            // 
+            this.homeButton.Location = new System.Drawing.Point(623, 18);
+            this.homeButton.Name = "homeButton";
+            this.homeButton.Size = new System.Drawing.Size(107, 34);
+            this.homeButton.TabIndex = 2;
+            this.homeButton.Text = "Home";
+            this.homeButton.UseVisualStyleBackColor = true;
+            this.homeButton.Click += new System.EventHandler(this.homeButton_Click);
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(349, 18);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(61, 34);
+            this.goButton.TabIndex = 1;
+            this.goButton.Text = "GO";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(12, 24);
+            this.addressTextBox.Multiline = true;
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(299, 22);
+            this.addressTextBox.TabIndex = 0;
+            this.addressTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.addressTextBox_KeyPress);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(800, 378);
+            this.webBrowser.TabIndex = 0;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainSplitContainer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(15, 15);
             this.Name = "MainForm";
             this.Text = "Browsarium";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.mainSplitContainer.Panel1.ResumeLayout(false);
+            this.mainSplitContainer.Panel1.PerformLayout();
+            this.mainSplitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).EndInit();
+            this.mainSplitContainer.ResumeLayout(false);
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Button refreshButton;
+
+        private System.Windows.Forms.Button homeButton;
+        private System.Windows.Forms.WebBrowser webBrowser;
+
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Button backwardButton;
+        private System.Windows.Forms.Button forwardButton;
+
+        private System.Windows.Forms.TextBox addressTextBox;
+
+        private System.Windows.Forms.SplitContainer mainSplitContainer;
 
         #endregion
     }
--- MainForm.cs
+++ MainForm.cs
@@ -1,20 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Browsarium
 {
-  public partial class MainForm : Form
-  {
-    public MainForm()
+    public partial class MainForm : Form
     {
-      InitializeComponent();
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            webBrowser.Navigate(addressTextBox.Text);
+        }
+
+        private void homeButton_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoHome();
+        }
+
+        private void backwardButton_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
+
+        private void forwardButton_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoForward();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            webBrowser.ScriptErrorsSuppressed = true;
+        }
+
+        private void refreshButton_Click(object sender, EventArgs e)
+        {
+            webBrowser.Refresh();
+        }
+
+        private void addressTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                addressTextBox.Text = addressTextBox.Text.Trim();
+                webBrowser.Navigate(addressTextBox.Text);
+            }
+        }
     }
-  }
 }
